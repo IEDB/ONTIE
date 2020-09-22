7079e3b2aea4044e39fb5b5a04ca45a3f5ce67cb
rctauber <rctauber@gmail.com>
2020-09-22 12:54:42 -0400

    Remove if statement

--- src/scripts/tree.sh
+++ src/scripts/tree.sh
@@ -8,8 +8,4 @@ URL="http://example.com?${QUERY_STRING}"
 ID=$(urlp --query --query_field=id "${URL}")
 ID="${ID:-owl:Thing}"
 
-if [[ ${ID} ]]; then
-	python3 -m gizmos.tree build/ontie.db ${ID}
-else
-	python3 -m gizmos.tree build/ontie.db owl:Thing
-fi
+python3 -m gizmos.tree build/ontie.db ${ID}
